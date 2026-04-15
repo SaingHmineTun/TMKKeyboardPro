5ad1051a12cd3e097528a444ea1db30b13fcb6df
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2026-04-15 08:59:12 +0630

    Added ShanLanguageTools into the project!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 }
 
 dependencies {
+    implementation("com.github.SaingHmineTun:ShanLanguageTools:1.0.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
